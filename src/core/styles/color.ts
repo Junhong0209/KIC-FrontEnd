e2413fbdfb632a28f8f7ce98d928cf8d173ad822
박준홍 <junh040209@naver.com>
2022-10-18 09:20:31 +0900

    style(css): Add colors and file input custom

--- src/core/styles/color.ts
+++ src/core/styles/color.ts
@@ -1,6 +1,9 @@
 export const colors = {
   Black: "#000000",
   White: "#FFFFFF",
+  BGray70: "#A3B3C2",
+  BGray30: "#3B4D5E",
+  Transparent: "transparent",
 
   ToastSuccess: "rgba(18, 24, 33, 08)",
   ToastError: "rgba(234, 31, 31, 0.8)",
